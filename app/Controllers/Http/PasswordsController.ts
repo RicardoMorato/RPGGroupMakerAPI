01059a293824b39e2bacec611da8b4df41f4e50f
Ricardo Morato <rmr@cin.ufpe.br>
2022-01-05 19:35:10 -0300

    feat: handle sending a html mail template

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -1,16 +1,23 @@
 import Mail from '@ioc:Adonis/Addons/Mail'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class PasswordsController {
   public async forgotPassword({ request, response }: HttpContextContract) {
-    const { email } = request.only(['email'])
+    const { email, resetPasswordUrl } = request.only(['email', 'resetPasswordUrl'])
+
+    const user = await User.findByOrFail('email', email)
 
     await Mail.send((message) => {
       message
         .from('no-reply@rpgtablemaker.com')
         .to(email)
         .subject('RPGTableMaker: Recuperação de senha')
-        .text('Clique no link abaixo para redefinir sua senha')
+        .htmlView('emails/forgotpassword', {
+          productName: 'RPGTableMaker',
+          name: user.username,
+          resetPasswordUrl,
+        })
     })
 
     return response.noContent()
