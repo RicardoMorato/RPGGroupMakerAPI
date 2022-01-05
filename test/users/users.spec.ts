d9f4555d1636fea796e073cd229280d3aa3c7f95
Ricardo Morato <rmr@cin.ufpe.br>
2022-01-05 13:12:21 -0300

    test: create and persist user

---
+++ test/users/users.spec.ts
@@ -0,0 +1,24 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Users', () => {
+  test.only('It should create a user', async (assert) => {
+    const userPayload = {
+      email: 'test@test.com',
+      username: 'testUser',
+      password: 'test@123',
+      avatar: 'https://images.com/image/1',
+    }
+
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+
+    assert.exists(body.user, 'User is undefined')
+    assert.exists(body.user.id, 'Id is undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.avatar, userPayload.avatar)
+    assert.equal(body.user.password, userPayload.password)
+  })
+})
