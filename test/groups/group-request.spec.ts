39e6267c261cd771ad874faf74f4cb8f6cf1e934
Ricardo Morato <rmr@cin.ufpe.br>
2022-01-07 05:21:17 -0300

    test: create a group request

---
+++ test/groups/group-request.spec.ts
@@ -0,0 +1,53 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import User from 'App/Models/User'
+import { GroupFactory, UserFactory } from 'Database/factories'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+let TOKEN = ''
+let USER = {} as User
+
+test.group('Group Request', (group) => {
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+
+  group.before(async () => {
+    const plainTextPassword = 'test@123'
+    const user = await UserFactory.merge({ password: plainTextPassword }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post('/sessions')
+      .send({ email: user.email, password: plainTextPassword })
+      .expect(201)
+
+    TOKEN = body.token.token
+    USER = user
+  })
+
+  group.after(async () => {
+    await supertest(BASE_URL).delete('/sessions').set('Authorization', `Bearer ${TOKEN}`)
+  })
+
+  test('It should create a group request', async (assert) => {
+    const master = await UserFactory.create()
+    const group = await GroupFactory.merge({ master: master.id }).create()
+
+    const { body } = await supertest(BASE_URL)
+      .post(`/groups/${group.id}/requests`)
+      .set('Authorization', `Bearer ${TOKEN}`)
+      .send({})
+      .expect(201)
+
+    assert.exists(body.groupRequest, 'Group Request is not defined')
+    assert.equal(body.groupRequest.userId, USER.id)
+    assert.equal(body.groupRequest.groupId, group.id)
+    assert.equal(body.groupRequest.status, 'PENDING')
+  })
+})
