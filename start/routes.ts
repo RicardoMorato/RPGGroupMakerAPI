53e5ab611d75ea2e0db8cee3c892965e58df9073
Ricardo Morato <rmr@cin.ufpe.br>
2022-01-07 23:43:57 -0300

    feat: add route prefix to all requests

--- start/routes.ts
+++ start/routes.ts
@@ -20,33 +20,32 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.group(() => {
+  Route.post('/users', 'UsersController.store')
+  Route.put('/users/:id', 'UsersController.update').middleware('auth')
 
-Route.post('/users', 'UsersController.store')
-Route.put('/users/:id', 'UsersController.update').middleware('auth')
+  Route.post('/forgot-password', 'PasswordsController.forgotPassword')
+  Route.post('/reset-password', 'PasswordsController.resetPassword')
 
-Route.post('/forgot-password', 'PasswordsController.forgotPassword')
-Route.post('/reset-password', 'PasswordsController.resetPassword')
+  Route.post('/sessions', 'SessionsController.store')
+  Route.delete('/sessions', 'SessionsController.destroy')
 
-Route.post('/sessions', 'SessionsController.store')
-Route.delete('/sessions', 'SessionsController.destroy')
+  Route.get('/groups', 'GroupsController.index').middleware('auth')
+  Route.post('/groups', 'GroupsController.store').middleware('auth')
+  Route.patch('/groups/:id', 'GroupsController.update').middleware('auth')
+  Route.delete('/groups/:id', 'GroupsController.destroy').middleware('auth')
+  Route.delete('/groups/:groupId/players/:playerId', 'GroupsController.removePlayer').middleware(
+    'auth'
+  )
 
-Route.get('/groups', 'GroupsController.index').middleware('auth')
-Route.post('/groups', 'GroupsController.store').middleware('auth')
-Route.patch('/groups/:id', 'GroupsController.update').middleware('auth')
-Route.delete('/groups/:id', 'GroupsController.destroy').middleware('auth')
-Route.delete('/groups/:groupId/players/:playerId', 'GroupsController.removePlayer').middleware(
-  'auth'
-)
-
-Route.get('/groups/:groupId/requests', 'GroupRequestsController.index').middleware('auth')
-Route.post('/groups/:groupId/requests', 'GroupRequestsController.store').middleware('auth')
-Route.post(
-  '/groups/:groupId/requests/:requestId/accept',
-  'GroupRequestsController.accept'
-).middleware('auth')
-Route.delete('/groups/:groupId/requests/:requestId', 'GroupRequestsController.destroy').middleware(
-  'auth'
-)
+  Route.get('/groups/:groupId/requests', 'GroupRequestsController.index').middleware('auth')
+  Route.post('/groups/:groupId/requests', 'GroupRequestsController.store').middleware('auth')
+  Route.post(
+    '/groups/:groupId/requests/:requestId/accept',
+    'GroupRequestsController.accept'
+  ).middleware('auth')
+  Route.delete(
+    '/groups/:groupId/requests/:requestId',
+    'GroupRequestsController.destroy'
+  ).middleware('auth')
+}).prefix('/api/v1')
